9b459cdf9f78ffc8dfb99caf615269dd0f998617
ekimlinger <ekimlinger@gmail.com>
2016-07-25 23:53:46 -0500

    Initial upload

---
+++ NodeServerSetup.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+#install all node components and modules
+#
+
+function installSeanStack(){
+   npm init -yes &&
+   npm install express path body-parser pg angular angular-route bootstrap grunt grunt-contrib-copy grunt-contrib-uglify grunt-contrib-jshint grunt-contrib-watch --save;
+}
+
+#
+# Creates standard node architecture
+#
+
+function createNodeFolders(){
+   echo "node_modules \n.DS_STORE \nnpm-debug.log \nplansToTakeOverTheWorld.md" > .gitignore;
+   cp ~/Documents/__groundZERO/Gruntfile.js .;
+   mkdir server;
+   cp ~/Documents/__groundZERO/server/app.js ./server/;
+   mkdir server/public;
+   mkdir server/public/assets;
+   mkdir server/public/assets/scripts;
+   mkdir server/public/assets/styles;
+   mkdir client/views;
+   cp ~/Documents/__groundZERO/server/views/index.html ./server/public/views/;
+   mkdir server/modules;
+   cp ~/Documents/__groundZERO/server/routes/index.js ./server/modules/;
+   mkdir client;
+   mkdir client/scripts;
+   cp ~/Documents/__groundZERO/client/app.js ./client/scripts/;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+#Shell Scripts
+
+This repository is to store all of my current shell scripts to make programming life easier. Instead of manually starting up a server or having to copy a different project folder, I've decided to write my own scripts to do it for me. Depending on the type of stack you want (currently only SEAN and MEAN stacks), it will install all the basic dependancies, directory structures, and files you need to start a server from scratch.
+
+##Installation
+
+Download or clone repository and simply run "installSeanStack" or "installMeanStack", and "installNodeFolders" from a terminal window where you would like the server installed.
+
+I'm definitely not the first one to do this, so if you have any suggestions or bug fixes be sure to let me know!
